c25e94cd429d8198e5b533e280be38ce020d5728
Slazaa <afitero@gmail.com>
2024-01-11 00:06:34 +0100

    Update storage

--- lib/clowder_ecs/src/storage.zig
+++ lib/clowder_ecs/src/storage.zig
@@ -4,10 +4,15 @@ const main = @import("main.zig");
 
 const Entity = main.Entity;
 
-pub fn Storage(comptime T: type) type {
-    const empty_struct = @sizeOf(T) == 0;
+pub const Error = error{
+    EntityAlreadyAdded,
+    InvalidEntity,
+};
 
-    const ComponentOrDummy = if (empty_struct) struct { dummy: u1 } else T;
+pub fn Storage(comptime Component: type) type {
+    const empty_struct = @sizeOf(Component) == 0;
+
+    const ComponentOrDummy = if (empty_struct) struct { dummy: u1 } else Component;
 
     return struct {
         const Self = @This();
@@ -29,5 +34,60 @@ pub fn Storage(comptime T: type) type {
 
             self.entities.deinit();
         }
+
+        pub fn has(self: Self, entity: Entity) bool {
+            return std.mem.containsAtLeast(Entity, self.entities.items, 1, entity);
+        }
+
+        pub fn getEntityIndex(self: Self, entity: Entity) !usize {
+            for (self.entities.items, 0..) |e, i| {
+                if (entity == e) return i;
+            }
+
+            return Error.InvalidEntity;
+        }
+
+        pub fn add(self: *Self, entity: Entity, component: Component) !void {
+            if (self.has(entity)) {
+                return Error.EntityAlreadyAdded;
+            }
+
+            if (!empty_struct) {
+                try self.instances.append(component);
+            }
+
+            try self.entities.append(entity);
+        }
+
+        pub fn remove(self: *Self, entity: Entity) bool {
+            if (!self.has(entity)) {
+                return false;
+            }
+
+            const entity_index = self.getEntityIndex(entity);
+
+            if (!empty_struct) {
+                self.instances.orderedRemove(entity_index);
+            }
+
+            self.entities.orderedRemove(entity_index);
+
+            return true;
+        }
+
+        pub usingnamespace if (!empty_struct)
+            struct {
+                pub fn get(self: Self, entity: Entity) ?Component {
+                    const entity_index = self.getEntityIndex(entity) catch return null;
+                    return self.instances.items[entity_index];
+                }
+
+                pub fn getPtr(self: Self, entity: Entity) ?Component {
+                    const entity_index = self.getEntityIndex(entity) catch return null;
+                    return &self.instances.items[entity_index];
+                }
+            }
+        else
+            struct {};
     };
 }
